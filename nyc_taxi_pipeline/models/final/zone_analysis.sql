8e6442c6c9c38c72fadad9a9d1e2bdcb2b59d36a
Minaanimmina <cracmina69@hotmail.fr>
2026-04-02 10:06:15 +0200

    feat: add monthly_summary, payment_analysis, enrich daily_summary, hourly_patterns and zone_analysis with lookup

--- nyc_taxi_pipeline/models/final/zone_analysis.sql
+++ nyc_taxi_pipeline/models/final/zone_analysis.sql
@@ -1,6 +1,10 @@
 SELECT
-    PULocationID,
+    z.PULocationID,
+    l.Zone AS zone_name,
+    l.Borough AS borough,
     COUNT(*) AS volume,
-    AVG(total_amount) AS revenus_moyens
-FROM {{ ref('stg_yellow_taxi_trips') }}
-GROUP BY PULocationID
+    AVG(z.total_amount) AS revenus_moyens
+FROM {{ ref('stg_yellow_taxi_trips') }} z
+LEFT JOIN {{ ref('taxi_zone_lookup') }} l
+    ON z.PULocationID = l.LocationID
+GROUP BY z.PULocationID, l.Zone, l.Borough
